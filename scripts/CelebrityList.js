cfe979484bd2fb738e8418fd1f4996aecf5b08c7
Kolby Simons <kolby.simons@gmail.com>
2025-06-10 15:40:38 -0500

    Completed

--- scripts/CelebrityList.js
+++ scripts/CelebrityList.js
@@ -1,20 +1,33 @@
-import { getCelebrities } from "./database.js"
+import { getCelebrities } from "./database.js";
 
-const celebrities = getCelebrities()
+const celebrities = getCelebrities();
+
+// when the celebrity name is clicked it should show the sport
+//setup event listener
+document.addEventListener("click", (clickEvent) => {
+  //check for data type
+  if (clickEvent.target.dataset.type === "celebrity") {
+    //show alert in browser using targeted dataset attributes
+    window.alert(
+      `${clickEvent.target.dataset.name} is a ${clickEvent.target.dataset.sport} star`
+    );
+  }
+});
 
 export const Celebrities = () => {
-    let html = "<ol>"
+  let html = "<ol>";
 
-    for (const star of celebrities) {
-        html += `<li 
+  for (const celebrity of celebrities) {
+    html += `<li 
+                    data-name="${celebrity.name}"
                     data-id="${celebrity.id}" 
                     data-type="celebrity"
-                    data-sport="${celebrty.sport}"
+                    data-sport="${celebrity.sport}"
                     id="star--${celebrity.id}">
                     ${celebrity.name}
-                </li>`
-    }
+                </li>`;
+  }
 
-    html += "</ol>"
-    return html
-}
+  html += "</ol>";
+  return html;
+};
--- scripts/Kids.js
+++ scripts/Kids.js
@@ -1,15 +1,25 @@
-import { getChildren } from "./database.js"
+import { getChildren } from "./database.js";
 
-const children = getChildren()
+const children = getChildren();
+
+//when the kids name is clicked it should show the wish
+//create event listener for click
+document.addEventListener("click", (event) => {
+  //check if data type child is clicked
+  if (event.target.dataset.type === "child") {
+    window.alert(
+      `${event.target.dataset.name}'s wish is ${event.target.dataset.wish}`
+    );
+  }
+});
 
 export const Kids = () => {
-    let html = "<ol>"
+  let html = "<ol>";
 
-    for (const child of children) {
-        html += `<li data-id="${child.id}" data-type="child" data-wish="${child.wish}">${child,name}</li>`
-    }
+  for (const child of children) {
+    html += `<li data-id="${child.id}" data-name="${child.name}" data-type="child" data-wish="${child.wish}">${child.name}</li>`;
+  }
 
-    html += "</ol>"
-    return html
-}
-
+  html += "</ol>";
+  return html;
+};
